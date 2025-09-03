bb3bfdfe603725fd0be8c3aa480fe17e2f41f6f6
Laurier Loiselle <lal@xiphos.com>
2025-09-03 09:11:19 -0400

    some copilot tweaks

--- src/init.lua
+++ src/init.lua
@@ -523,6 +523,26 @@ vim.keymap.set("n", ",pg", "<cmd>NeovimProjectDiscover<CR>", { desc = "Discover 
 vim.keymap.set("n", ",ph", "<cmd>NeovimProjectHistory<CR>", { desc = "Project History" })
 vim.keymap.set("n", ",pl", "<cmd>NeovimProjectLoadRecent<CR>", { desc = "Load recent Project" })
 
+-- Magic Tab
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+cmp.setup({
+    mapping = {
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+                fallback()
+            end
+        end),
+    },
+})
+
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+
 -- Lspkind
 local lspkind = require('lspkind')
 cmp.setup {
